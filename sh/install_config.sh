9d22a67f925e99bce572aa544ca843a3aeab87eb
Mark Morgan <momipsl@ipsl.jussieu.fr>
2017-02-10 13:14:32 +0100

    Fixed old paths

--- sh/install_config.sh
+++ sh/install_config.sh
@@ -6,8 +6,8 @@ source $ERRATA_WS_HOME/sh/utils.sh
 # Main entry point.
 main()
 {
-	cp $ERRATA_WS_HOME/resources/template-supervisord.conf $ERRATA_WS_HOME/ops/config/supervisord.conf
-	cp $ERRATA_WS_HOME/resources/template-ws.conf $ERRATA_WS_HOME/ops/config/ws.conf
+	cp $ERRATA_WS_HOME/resources/supervisord.conf $ERRATA_WS_HOME/ops/config/supervisord.conf
+	cp $ERRATA_WS_HOME/resources/ws.conf $ERRATA_WS_HOME/ops/config/ws.conf
 
 	log "configuration files initialized"
 }
