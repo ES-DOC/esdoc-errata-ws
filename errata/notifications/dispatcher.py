497be85c59e4ffe33b97c130b8aeb5e9314e70a0
Mark A. Greenslade <asladeofgreen@pm.me>
2022-12-15 17:30:15 +0100

    1. Added smtplib to dispatcher.

--- errata/notifications/dispatcher.py
+++ errata/notifications/dispatcher.py
@@ -4,19 +4,48 @@ from errata.notifications import constants
 from errata.notifications import templates
 
 
-def dispatch_on_accepted(errata_uid):
+
+HOST = "smtp.de.opalstack.com"
+CREDENTIALS = ("errata-moderation", "opalstack@Silence93!")
+PORT_SSL = 465
+PORT_TLS = 587
+SENDER = "errata-moderation@es-doc.org"
+RECIEVERS = "asladeofgreen@gmail.com"
+
+
+def dispatch_on_accepted(email_address, errata_uid):
 	email_body = templates.get_on_accepted_template(errata_uid)
+	
+	_dispatch(email_address, email_body)
 
-	return email_body
 
-
-def dispatch_on_proposed(errata_uid):
+def dispatch_on_proposed(email_address, errata_uid):
 	email_body = templates.get_on_proposed_template(errata_uid)
 
-	return email_body
+	_dispatch(email_address, email_body)
 
 
-def dispatch_on_rejected(errata_uid):
+def dispatch_on_rejected(email_address, errata_uid):
 	email_body = templates.get_on_rejected_template(errata_uid)
 
-	return email_body
+	_dispatch(email_address, email_body)
+
+
+def _dispatch(email_address, email_body):
+	"""Dispatches an email message.
+	
+	"""
+	# Connect over TLS port.
+	smtp = smtplib.SMTP(HOST, PORT_TLS)
+
+	# Secure contents with TLS encryption.
+	smtp.starttls()
+
+	# Connect.
+	smtp.login(CREDENTIALS[0], CREDENTIALS[1])
+
+	# Dispatch email.
+	smtp.sendmail(SENDER, [email_address], email_body)
+
+	# Close smtp channel.
+	smtp.quit()
